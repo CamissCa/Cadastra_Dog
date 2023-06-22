7cbf22ea5afb2281c4e9c634afd0367af8e0a418
CamissCa <camila.cardososenai@gmail.com>
2023-06-21 22:18:16 -0300

    Cadastra_dog 1.0

---
+++ Models/cachorro.cs
@@ -0,0 +1,9 @@
+namespace Dogs.Models{
+    public class Cadastro_dog
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public string age { get; set; } = string.Empty;
+    }
+    
+}
